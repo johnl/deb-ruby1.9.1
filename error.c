13bc52fb5a13b270a800a5faa3d3cd2d96df79be
James Healy <jimmy@deefa.com>
2012-05-17 15:41:21 +1000

    Imported Upstream version 1.9.3.194

--- error.c
+++ error.c
@@ -1128,15 +1128,13 @@ syserr_initialize(int argc, VALUE *argv, VALUE self)
     else err = "unknown error";
     if (!NIL_P(mesg)) {
 	rb_encoding *le = rb_locale_encoding();
-	VALUE str = mesg;
+	VALUE str = StringValue(mesg);
+	rb_encoding *me = rb_enc_get(mesg);
 
-	StringValue(str);
 	mesg = rb_sprintf("%s - %.*s", err,
 			  (int)RSTRING_LEN(str), RSTRING_PTR(str));
-	if (le == rb_usascii_encoding()) {
-	    rb_encoding *me = rb_enc_get(mesg);
-	    if (le != me && rb_enc_asciicompat(me))
-		le = me;
+	if (le != me && rb_enc_asciicompat(me)) {
+	    le = me;
 	}/* else assume err is non ASCII string. */
 	OBJ_INFECT(mesg, str);
 	rb_enc_associate(mesg, le);
@@ -1635,11 +1633,31 @@ make_errno_exc(const char *mesg)
     return rb_syserr_new(n, mesg);
 }
 
+static VALUE
+make_errno_exc_str(VALUE mesg)
+{
+    int n = errno;
+
+    errno = 0;
+    if (!mesg) mesg = Qnil;
+    if (n == 0) {
+	const char *s = !NIL_P(mesg) ? RSTRING_PTR(mesg) : "";
+	rb_bug("rb_sys_fail_str(%s) - errno == 0", s);
+    }
+    return rb_syserr_new_str(n, mesg);
+}
+
 VALUE
 rb_syserr_new(int n, const char *mesg)
 {
     VALUE arg;
     arg = mesg ? rb_str_new2(mesg) : Qnil;
+    return rb_syserr_new_str(n, arg);
+}
+
+VALUE
+rb_syserr_new_str(int n, VALUE arg)
+{
     return rb_class_new_instance(1, &arg, get_syserr(n));
 }
 
@@ -1650,9 +1668,21 @@ rb_syserr_fail(int e, const char *mesg)
 }
 
 void
+rb_syserr_fail_str(int e, VALUE mesg)
+{
+    rb_exc_raise(rb_syserr_new_str(e, mesg));
+}
+
+void
 rb_sys_fail(const char *mesg)
 {
     rb_exc_raise(make_errno_exc(mesg));
+}
+
+void
+rb_sys_fail_str(VALUE mesg)
+{
+    rb_exc_raise(make_errno_exc_str(mesg));
 }
 
 void
@@ -1664,9 +1694,25 @@ rb_mod_sys_fail(VALUE mod, const char *mesg)
 }
 
 void
+rb_mod_sys_fail_str(VALUE mod, VALUE mesg)
+{
+    VALUE exc = make_errno_exc_str(mesg);
+    rb_extend_object(exc, mod);
+    rb_exc_raise(exc);
+}
+
+void
 rb_mod_syserr_fail(VALUE mod, int e, const char *mesg)
 {
     VALUE exc = rb_syserr_new(e, mesg);
+    rb_extend_object(exc, mod);
+    rb_exc_raise(exc);
+}
+
+void
+rb_mod_syserr_fail_str(VALUE mod, int e, VALUE mesg)
+{
+    VALUE exc = rb_syserr_new_str(e, mesg);
     rb_extend_object(exc, mod);
     rb_exc_raise(exc);
 }
